53066493544e3f625e7a8c45d66a334374f740d2
KevinED11 <mendez_kael@hotmail.com>
2023-01-28 16:11:52 -0700

    update password

--- password.py
+++ password.py
@@ -2,16 +2,20 @@ from string import ascii_letters, digits, punctuation
 from secrets import choice
 from passlib.hash import bcrypt
 from zxcvbn import zxcvbn
+
 def gen_password():
     characters_pass = ascii_letters + digits + punctuation
-    return str(''.join(choice(characters_pass) for _ in range(12)))
+    password =  str(''.join(choice(characters_pass) for _ in range(12)))
+    pass_security = zxcvbn(password)
+
+    while pass_security['score'] < 3:
+        password = gen_password()
+
+    return password
+
 
 def encrypt_password(password: str):
     return bcrypt.hash(password)
-
-def pass_secure(password: str):
-    result = zxcvbn(password)
-    return result
 
 def verify_password(password: str, hashed_password: str):
     return bcrypt.verify(password, hashed_password)
@@ -19,4 +23,3 @@ def verify_password(password: str, hashed_password: str):
 if __name__ == '__main__':
     print(gen_password())
     print(encrypt_password(gen_password()))
-    print(pass_secure(gen_password()))
