d8bd4df6e1257e288ff2ab28ba32703b5a3dbe39
KevinED11 <mendez_kael@hotmail.com>
2023-01-28 10:26:14 -0700

    added password_secure function

--- password.py
+++ password.py
@@ -1,13 +1,17 @@
 from string import ascii_letters, digits, punctuation
 from secrets import choice
 from passlib.hash import bcrypt
-
+from zxcvbn import zxcvbn
 def gen_password():
     characters_pass = ascii_letters + digits + punctuation
-    return str(''.join(choice(characters_pass) for _ in range(10)))
+    return str(''.join(choice(characters_pass) for _ in range(12)))
 
 def encrypt_password(password: str):
     return bcrypt.hash(password)
+
+def pass_secure(password: str):
+    result = zxcvbn(password)
+    return result
 
 def verify_password(password: str, hashed_password: str):
     return bcrypt.verify(password, hashed_password)
@@ -15,3 +19,4 @@ def verify_password(password: str, hashed_password: str):
 if __name__ == '__main__':
     print(gen_password())
     print(encrypt_password(gen_password()))
+    print(pass_secure(gen_password()))
