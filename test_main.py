62d27502546ca810fd4db5e19fe120fd9bc6128e
KevinED11 <mendez_kael@hotmail.com>
2023-01-31 08:56:30 -0700

    add progress test

--- test_main.py
+++ test_main.py
@@ -22,7 +22,24 @@ class TestClass:
         assert all(isinstance(i, User) for i in users_list_obj)
         print(f'json a obj User: {users_list_obj}')
 
+    def test_create_user(self):
+        header = {'Accept': 'application/json'}
+        response = post("http://localhost/users", headers=header)
+        assert  response.status_code == 200
+        new_user = response.json()
+        assert new_user['id'] in new_user and new_user['username'] and new_user['password'] and new_user['age']
+        print(new_user)
 
+    def test_get_user_by_id(self):
+        random_id = uuid4()
+        response = get(f"http://localhost:8000/users/{random_id}")
+        assert response.status_code == 404, 'User not found'
+        assert response.status_code == 200
+
+        user_response_dict: Dict = response.json()
+        print(user_response_dict)
+        obj_user: User = User(**user_response_dict)
+        assert isinstance(obj_user, User)
 
 
 
