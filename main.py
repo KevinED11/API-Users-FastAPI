318524d616e4e7d8ad33e2484ff16d47472bcded
KevinED11 <mendez_kael@hotmail.com>
2023-01-31 00:46:50 -0700

    added validations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Response
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validator
 from typing import Optional, List, Dict
 from uuid import uuid4, UUID
 from datetime import datetime
@@ -17,8 +17,18 @@ class User(BaseModel):
     password: str
     created_at: datetime
 
+    #@validator("age")
+    #def validate_age(cls, age):
+     #   if age < 18:
+      #      raise HTTPException(status_code=400, detail="Place age more than 18 characters")
+    #@validator("password")
+    #def validate_password(cls, v):
+     #   if len(v) < 12:
+      #      raise ValueError('Place 12 or more characters')
+       # return v
 
-users: List[User] = [
+
+users_ls: List[User] = [
 
 
 ]
@@ -34,7 +44,7 @@ async def read_root():
 
 @app.get('/users', response_model = List[User])
 async def get_users(name: Optional[str] = None, age: Optional[int] = None):
-    filtered_users = users
+    filtered_users = users_ls
 
     if name and age:
         filtered_users = [user for user in filtered_users if user.name == name and user.age == age]
@@ -51,10 +61,10 @@ async def create_user(user: User):
     user.created_at = datetime.now()
     user.password = encrypt_password(gen_password())
 
-    if user.age < 18:
-        raise HTTPException(status_code=400, detail='User is under age(place number >= 18)')
+    #if user.age < 18:
+        #raise HTTPException(status_code=400, detail='User is under age(place number >= 18)')
 
-    for existing_user in users:
+    for existing_user in users_ls:
         if user.email == existing_user.email and user.username == existing_user.username:
             raise HTTPException(status_code=400, detail=f'User with email ({user.email}) and username ({user.username}) already exists')
         elif user.username == existing_user.username:
@@ -62,9 +72,9 @@ async def create_user(user: User):
         elif user.email == existing_user.email:
             raise HTTPException(status_code=400, detail=f' User with email ({user.email}) already exists')
 
-    users.append(user)
+    users_ls.append(user)
     users_id_dict[user.id] = user
-    return Response(status_code=201, content=str({'message': 'user created successfully', 'user_id': user.id, 'username': user.name}))
+    return Response(status_code=201, content=str({'message': 'user created successfully', 'user_id': user.id, 'username': user.name, 'password': user.password}))
 
 @app.get('/users/{user_id}', response_model = User)
 async def get_user_by_id(user_id: UUID):
@@ -82,7 +92,7 @@ async def get_user_by_id(user_id: UUID):
 async def delete_user(user_id: UUID):
     if user_id in users_id_dict:
         user = users_id_dict.pop(user_id)
-        users.remove(user)
+        users_ls.remove(user)
         return Response(status_code=204, content=None)
     raise HTTPException(status_code=404, detail='User not found')
 
@@ -92,7 +102,25 @@ async def delete_user(user_id: UUID):
             #return Response(content=None, status_code=204)
     #raise HTTPException(status_code=404, detail='User not found')
 
-@app.put('users/{user_id}')
-async def update_user():
-    pass
+@app.put('users/{user_id}', response_model= User)
+async def update_user(user_id: UUID, updated_user: User):
+    if user_id not in users_id_dict:
+        raise HTTPException(status_code=404, detail=f'User with id {user_id} not found')
+
+    user: User = users_id_dict[user_id]
+    user.username = updated_user.username
+    user.name = updated_user.name
+    user.email = updated_user.email
+    user.surname = updated_user.surname
+    user.age = updated_user.age
+
+    users_id_dict[user_id] = user
+    for i, existing_user in enumerate(users_ls):
+        if existing_user.id == user_id:
+            users_ls[i] = user
+
+    return user
+
+
+
 
