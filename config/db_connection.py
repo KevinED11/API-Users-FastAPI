026a2b5b10e236d2864d80e33855082630a98232
KevinED11 <mendez_kael@hotmail.com>
2023-02-02 23:06:53 -0700

    added .env file

--- config/db_connection.py
+++ config/db_connection.py
@@ -1,4 +1,4 @@
 from sqlmodel import create_engine
-
-engine = create_engine("postgresql+psycopg2://postgres:root@localhost:5432/postgres", echo=True)
+import os
+engine = create_engine("postgresql+psycopg2://postgres:root@localhost:5432/postgres")
 
--- config/init_db.py
+++ config/init_db.py
@@ -6,15 +6,14 @@ from sqlmodel import SQLModel, Session
 def create_db_and_table():
     SQLModel.metadata.create_all(engine)
 
-
-def create_new_users():
-
+def save_table_in_db():
     session = Session(engine)
     session.commit()
+    session.close()
 
 def main():
     create_db_and_table()
-    create_new_users()
+    save_table_in_db()
 
 if __name__ == '__main__':
     main()
