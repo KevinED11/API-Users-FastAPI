3ff757f3728d2f6bd346e00e8fa12268e051bd39
KevinED11 <mendez_kael@hotmail.com>
2023-02-04 11:27:44 -0700

    undefined

--- routes/users/user_update_field.py
+++ routes/users/user_update_field.py
@@ -1,9 +1,29 @@
-from routes.users.user_creation import APIRouter
+from fastapi import APIRouter, HTTPException
 from typing import Optional
 from routes.users.user_find import UUID
+from routes.users.root_get import users_id_dict, users_ls
 
 userUpdateField = APIRouter()
 
 @userUpdateField.patch('/users/{user_id}', response_model=None, status_code=200)
 async def field_update(user_id: UUID, name: Optional[str] = None, surname: Optional[str] = None, email: Optional[str] = None ):
-    pass
+    if user_id not in users_id_dict:
+        raise HTTPException(status_code=404, detail=f'user with ID {user_id} not found')
+
+    user = users_id_dict[user_id]
+
+    if name:
+        user.name = name
+    if surname:
+        user.surname = surname
+    if email:
+        user.email = email
+
+
+    users_ls.append(user)
+    users_id_dict[user.id_user] = user
+
+
+
+
+
